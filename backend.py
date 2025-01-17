ac87ea88c9b478fd3e88c9c4a3ba309ab71bab1e
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-01-17 07:56:58 +0100

    update goals of the backend project

--- backend.py
+++ backend.py
@@ -1,11 +1,13 @@
 from flask import Flask 
+import csv
+import os
 
 
 app = Flask(__name__) 
 
  
 
-@app.route("add")
+@app.route("")
 def add_location(input):
     with open("locations.txt", "1") as myfile:
             myfile.write(input)
